0631a288236d3f10b77d190781cb1cc3543ef91e
nibaldo <nibaldo.donoso@cosmotech.com>
2023-09-14 11:15:31 +0200

    allow change pass

--- vault/addpolicies.sh
+++ vault/addpolicies.sh
@@ -2,6 +2,7 @@
 # param $1: organization_name
 # param $2: tenant_id
 
+vault auth enable -path=userpass-$1 userpass
 vault policy write $1_superadmin -<<EOF
 path "$1/$2/*" {
   capabilities = ["create", "update", "patch", "read", "delete", "list"]
@@ -11,6 +12,12 @@ path "organization/$1" {
 }
 path "$1/*" {
   capabilities = ["create", "update", "patch", "read", "delete", "list"]
+}
+path "auth/userpass-$1/users/*" {
+  capabilities = ["update"]
+  allowed_parameters = {
+    "password" = []
+  }
 }
 EOF
 
@@ -24,7 +31,15 @@ path "organization/$1" {
 path "$1/*" {
   capabilities = ["create", "update", "patch", "read", "list"]
 }
+path "auth/userpass-$1/users/*" {
+  capabilities = ["update"]
+  allowed_parameters = {
+    "password" = []
+  }
+}
 EOF
+
+accessor=$(vault auth list | awk '$1 ~ /'$1'/ {print $3}' -)
 
 vault policy write $1_user -<<EOF
 path "$1/$2/users/{{identity.entity.metadata.email}}/*" {
@@ -33,6 +48,14 @@ path "$1/$2/users/{{identity.entity.metadata.email}}/*" {
 path "$1/$2/projects/{{identity.entity.metadata.project}}/*" {
   capabilities = ["create", "update", "patch", "read", "delete", "list"]
 }
+
+path "auth/userpass-$1/users/{{identity.entity.aliases.$accessor.name}}" {
+  capabilities = ["update"]
+  allowed_parameters = {
+    "password" = []
+  }
+}
+
 path "organization/$1" {
   capabilities = ["read", "list"]
 }
