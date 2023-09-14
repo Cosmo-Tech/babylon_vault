a2b6e0c70b78473d385368886dd3abd926b1717d
nibaldo <nibaldo.donoso@cosmotech.com>
2023-09-14 10:43:42 +0200

    update policies

--- vault/addpolicies.sh
+++ vault/addpolicies.sh
@@ -1,45 +1,54 @@
 #!/bin/bash
-# param 1: organization_name
-# param 2: tenant_id
+# param $1: organization_name
+# param $2: tenant_id
 
 vault policy write $1_superadmin -<<EOF
 path "$1/$2/*" {
-  capabilities = ["create", "update", "patch", "read", "delete"]
+  capabilities = ["create", "update", "patch", "read", "delete", "list"]
 }
 path "organization/$1" {
-  capabilities = ["create", update", "patch", "read", "delete"]
+  capabilities = ["create", "update", "patch", "read", "delete", "list"]
+}
+path "$1/*" {
+  capabilities = ["create", "update", "patch", "read", "delete", "list"]
 }
 EOF
 
 vault policy write $1_admin -<<EOF
 path "$1/$2/*" {
-  capabilities = ["create", "update", "patch", "read"]
+  capabilities = ["create", "update", "patch", "read", "list"]
 }
 path "organization/$1" {
-  capabilities = ["create", "read"]
+  capabilities = ["create", "update", "patch", "read", "list"]
+}
+path "$1/*" {
+  capabilities = ["create", "update", "patch", "read", "list"]
 }
 EOF
 
 vault policy write $1_user -<<EOF
 path "$1/$2/users/{{identity.entity.metadata.email}}/*" {
-  capabilities = ["create", "update", "patch", "read", "delete"]
+  capabilities = ["create", "update", "patch", "read", "delete", "list"]
 }
 path "$1/$2/projects/{{identity.entity.metadata.project}}/*" {
-  capabilities = ["create", "update", "patch", "read", "delete"]
+  capabilities = ["create", "update", "patch", "read", "delete", "list"]
 }
 path "organization/$1" {
-  capabilities = ["read"]
+  capabilities = ["read", "list"]
 }
 path "$1/$2/platform/*" {
-  capabilities = ["read"]
+  capabilities = ["read", "list"]
 }
 path "$1/$2/global/*" {
-  capabilities = ["read"]
+  capabilities = ["read", "list"]
 }
 path "$1/$2/babylon/*" {
-  capabilities = ["read"]
+  capabilities = ["read", "list"]
 }
 path "$1/$2/*" {
-  capabilities = ["read"]
+  capabilities = ["read", "list"]
+}
+path "$1/*" {
+  capabilities = ["read", "list"]
 }
 EOF
